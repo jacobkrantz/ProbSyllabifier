22a1d2587c337756ac455c81ebd1adb3e1fffb30
jacobkrantz <jkrantz@zagmail.gonzaga.edu>
2017-02-08 21:41:02 -0800

    begun work on utils for HMM. Finished import of syllabDict file

---
+++ HMM.py
@@ -0,0 +1,22 @@
+from utils import Utils
+
+# utils.importSyllabDict("./corpusFiles/topSyllabDict.txt")
+class HMM:
+
+    def __init__(self):
+        self.utils = Utils()
+
+        self.syllabDict = {}
+
+        self.syllableLst = []
+        self.sylBigramLst = []
+        self.sylBigramDict = {}
+        self.matrixA
+
+
+    def buildMatrixA(self):
+
+
+
+    def buildMatrixB(self):
+        return 
