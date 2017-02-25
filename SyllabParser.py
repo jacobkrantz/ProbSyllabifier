d7d0ccf16435324744fa54ddc9a7ca60ebf9646d
mdulin2 <mdulin2@zagmail.gonzaga.edu>
2017-02-24 17:47:42 -0800

    Module for the parser is now working. Haven't had all of the time in the world to test it but the first 8 test cases I did worked well. Let me know if this isn't the format that you want the list in

--- SyllabParser.py
+++ SyllabParser.py
@@ -15,18 +15,34 @@ Date Modified:  2/23/17
 class SyllabParser:
 
     def __init__(self):
-        self.testInt = 0
-        self.bigramLst = []
+        #spot on the line
+        self.spot = 0
+        self.countLines = 0
         self.initialList = []
-        self.inFile = ""
-    def bringInFile(self,inputFile):
+        self.bigramLst = []
+        #the string line from the file inputted
+        self.line = ''
+
+    ##brings in the contents of the file
+    ##Puts them into initialList, a class variable
+    def __bringInFile(self,inputFile):
 
         with open(inputFile) as f:
             for line in f:
                 self.initialList.append(line)
+                self.countLines+=1
 
-        print self.initialList[1]
-    def makeParseWord(self, index):
+    #Creates the list of bigrams
+    def makeBiagramList(self):
+
+        self.__bringInFile("./HMMFiles/SyllabDict.txt")
+        #self.__makeParseWord(1)
+        for i in range(self.countLines):
+            self.__makeParseWord(i)
+    #Parses a word into bigrams and whether there was a boundary between them.
+    #In the format (firstPhone,secondPhone,boundary)
+    #if there is a boundary, the value is a 1. If there is no boundary the value is 0.
+    def __makeParseWord(self, index):
         boundary = False
         phoneLst = []
         line = self.initialList[index]
@@ -34,22 +50,54 @@ class SyllabParser:
         #takes out the beginning word in the dictionary
         while(line[0] != ' '):
             line = line[1:]
-        line = line[1:]
-        for i in range(len(line)):
-            if(self.phoneCheck(line,i)):
-                print("test")
+        self.line = line[1:]
+
+        oldPhone = self.__findNextPhone()
+        while(self.spot < len(line)):
+            #get the phone
+            phone = self.__findNextPhone()
+            #knowing that the word has ended
+            value = self.__boundaryFound(phone,oldPhone)
+
+            if(phone == '\0' and self.spot == 0):
+                break
+            else:
+                self.bigramLst.append([oldPhone,phone,value])
+            oldPhone = phone
 
 
-    def phoneCheck(self, line, index):
-        if(line[index-1] != ' '):
-            return True
-        return False
 
-def main():
+    #takes in a line that has been cropped out
+    #returns true if a boundary is found between the two phone
+    #and false otherwise
+    def __boundaryFound(self,phone,oldPhone):
+        setString = ''
+        for i in range (2,len(self.line)):
+            if(self.line[self.spot-i] != oldPhone[0]):
+                setString = self.line[self.spot-i]+setString
+            else:
+                break
+        if('[' in setString):
+            return 1;
+        else:
+            return 0;
 
-    newParser = SyllabParser()
-    newParser.bringInFile("./HMMFiles/SyllabDict.txt")
-    newParser.makeParseWord(1)
-    return 0
 
-main()
+    #Takes in a line that has been cropped down
+    #to find the next phone in the line and the number in the line.
+    #returns the next phone in the line
+    def __findNextPhone(self):
+        phone = ""
+        while(self.spot < len(self.line)):
+            if(self.line[self.spot].isalpha()):
+                phone += self.line[self.spot]
+                if(self.line[self.spot+1].isalpha() == False):
+                    self.spot+=1
+                    return phone
+
+            self.spot +=1
+
+        #resets the value of spot to be used later.
+        self.spot = 0
+        #used to end the loop in makeParseWord
+        return '\0'
