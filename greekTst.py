07cacebc6393443c031b9a1c19cf9e412233ae4f
mdulin2 <mdulin2@zagmail.gonzaga.edu>
2017-05-05 11:30:34 -0700

    I added sa file that prints the greek letters

---
+++ greekTst.py
@@ -0,0 +1,11 @@
+def main():
+
+   
+    LatinCapitalAE = u'\u00C6'
+    print LatinCapitalAE
+    CapitalGamma = u'\u0393'
+    print CapitalGamma
+    SmallGamma = u'\u03b3'
+    print SmallGamma
+
+main()
