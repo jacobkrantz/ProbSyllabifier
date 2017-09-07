ec0e1ef0123bb06ebe8f66e11ca2805876fecd63
jacobkrantz <jkrantz@zagmail.gonzaga.edu>
2017-09-07 12:16:07 -0700

    initial GA commit

---
+++ GeneticAlgorithm/Chromosome.py
@@ -0,0 +1,55 @@
+'''
+fileName:       Chromosome.py
+Authors:        Jacob Krantz
+Date Modified:  9/7/17
+
+Contains
+- definitions and properties of a chromosome
+- methods for modifying the chromosome
+
+*Chromosomes are phone transcription schemes
+'''
+
+class Chromosome:
+
+    # structure of chromosome is still in flux
+    def __init__(self, numCategories):
+        self.genes = self.initGenes(numCategories)
+        self.fitness = 0.00
+
+    # for sorting a collection of chromosomes. Pythonic beauty.
+    def __lt__(self, other):
+        return self.fitness < other.fitness
+
+    def initGenes(self, numCategories):
+        genes = []
+        for i in range(numCategories):
+            genes.append([])
+        return genes
+
+    def getGenes(self):
+        return self.genes
+
+    # inserts a gene into a specified category.
+    # removes gene from previous category it was in.
+    def insertIntoCategory(self, categoryNumber, gene):
+        self.removeGene(gene)
+        self.genes[categoryNumber].append(gene)
+
+    # should the individual chromosome fitness be calculated
+    # here or in GeneticAlgorithm?
+    def comuteFitness(self):
+        pass
+
+    #----------------#
+    #   "Private"    #
+    #----------------#
+
+    # removes a gene from the population.
+    # returns True if gene is found and removed.
+    def removeGene(self, gene):
+        for i in range(0, len(self.genes)):
+                if gene in self.genes[i]:
+                    self.genes[i].remove(gene)
+                    return True
+        return False
--- nist/__init__.py
+++ nist/__init__.py
@@ -1,4 +1,3 @@
 from testing import CompareNIST
-#from syllabTools import SyllabTools
 from nistClient import NISTClient
 from nist import NIST
