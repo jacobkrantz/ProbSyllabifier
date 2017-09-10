119d5611b3014e7259e206ea6dba0628267b7626
jacobkrantz <jkrantz@zagmail.gonzaga.edu>
2017-09-10 00:24:44 -0700

    Output evolution logs

--- GeneticAlgorithm/GeneticAlgorithm.py
+++ GeneticAlgorithm/GeneticAlgorithm.py
@@ -1,6 +1,6 @@
 from celex import CELEX
 from Chromosome import Chromosome
-from Mating import Mate
+from Mating import Mating
 from random import randint
 '''
 fileName:       GeneticAlgorithm.py
@@ -18,26 +18,29 @@ class GeneticAlgorithm:
         self.population = []
         self.config = config
         self.celex = CELEX()
+        self.mating = Mating(config)
 
     # displays all GeneticAlgorithm parameters to the console
     def displayParameters(self):
-        ips = self.config["InitialPopulationSize"]
-        ps  = self.config["PopulationSize"]
-        mf  = self.config["MutationFactor"]
-        ne  = self.config["NumEvolutions"]
-        noc = self.config["NumCategories"]
-        nog = len(self.config["GeneList"])
+        ips  = self.config["InitialPopulationSize"]
+        ps   = self.config["PopulationSize"]
+        nomp = self.config["NumMatingPairs"]
+        mf   = self.config["MutationFactor"]
+        ne   = self.config["NumEvolutions"]
+        noc  = self.config["NumCategories"]
+        nog  = len(self.config["GeneList"])
 
         displayString = """
     Genetic Algorithm Parameters
     ----------------------------
     Initial Population Size  %s
     Population Size          %s
+    Number of Mating Pairs   %s
     MutationFactor           %s
     Number of Evolutions     %s
     Number of Categories     %s
     Number of Genes          %s
-    """ % (ips, ps, mf, ne, noc, nog)
+    """ % (ips, ps, nomp, mf, ne, noc, nog)
 
         print(displayString)
 
@@ -52,10 +55,22 @@ class GeneticAlgorithm:
                 newChromosome.insertIntoCategory(randomCategory, gene)
 
             self.population.append(newChromosome)
-        self.computeFitness()
+        self.__computeFitness()
+        self.__displayPopulation(0)
 
+    # 1 evolution: mate the population, mutates them, computes their accuracy,
+    # sort by accuracy, and save the best to the file.
+    # Repeat evolutions as specified in config.json.
     def evolve(self):
-        pass
+        evolutionCount = 0
+        while evolutionCount < self.config["NumEvolutions"]:
+            self.population = self.mating.crossover(self.population)
+            self.__mutate()
+            self.__computeFitness()
+            self.__sort()
+            self.__saveMostFitChromosome(evolutionCount)
+            self.__displayPopulation(evolutionCount)
+            evolutionCount += 1
 
     #----------------#
     #   "Private"    #
@@ -64,7 +79,7 @@ class GeneticAlgorithm:
     # compute the fitness of all chromosomes in the population by
     #   running the ProbSyllabifier.
     # sets Chromosome.fitness equal to syllabification accuracy.
-    def computeFitness(self):
+    def __computeFitness(self):
         trainSize = self.config["TrainingSizeHMM"]
         testSize = self.config["TestingSizeHMM"]
         for i in range(len(self.population)):
@@ -75,15 +90,30 @@ class GeneticAlgorithm:
 
     # sort self.population by fitness (syllabification accurracy)
     # ordering: highest (self.population[0]) -> lowest
-    def sort(self):
-        self.population.sort().reverse()
+    def __sort(self):
+        self.population.sort()
+        self.population.reverse()
 
     # keeps the population from getting stagnant by moving around genes
     # in the chromosome pseudo-randomly
-    def mutate(self):
+    def __mutate(self):
         pass
 
-    # best chromosome should be saves to an aptly named file
-    # after each evolution.
-    def saveMostFitChromosome(self):
-        pass
+    # Best chromosome saved in "GeneticAlgorithm/EvolutionLogs".
+    # Each line is a category.
+    def __saveMostFitChromosome(self, curEvolution):
+        bestChromosome = self.population[0]
+        location = "GeneticAlgorithm/EvolutionLogs/"
+        prefix = "evo" + str(curEvolution) + "-"
+        fitness = str(bestChromosome.getFitness())
+        fileName = location + prefix + fitness + ".log"
+
+        with open(fileName, 'w') as outFile:
+            for category in bestChromosome.getGenes():
+                outFile.write(' '.join(category) + '\n')
+
+    def __displayPopulation(self, evolutionNumber):
+        print("Population after evolution #" + str(evolutionNumber))
+        for i in range(len(self.population)):
+            print("chrom" + str(i) + "\t" + str(self.population[i].getFitness()))
+        print
